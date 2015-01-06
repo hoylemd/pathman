c8a86468ec53b2862dbf64bdc1cad1ad28592106
Michael D. Hoyle <hoylemd@gmail.com>
2015-01-06 08:47:48 -0500

    moar pep8

--- permissions.py
+++ permissions.py
@@ -1,14 +1,15 @@
 from rest_framework import permissions
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-  """
-  Custom permission to only allow owners of an object to edit it
-  """
-  def has_object_permission(self, request, view, obj):
-    # Read permissions to any request
-    # so we'll always allow GET, HEAD, or OPTIONS
-    if request.method in permissions.SAFE_METHODS:
-      return True
 
-    # Write permissions to owners only
-    return obj.owner == request.user
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions to any request
+        # so we'll always allow GET, HEAD, or OPTIONS
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions to owners only
+        return obj.owner == request.user
