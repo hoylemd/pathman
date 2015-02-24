02dcdc5f9736bd2e64b96dbb29b495fbcc59eb19
Michael D. Hoyle <hoylemd@gmail.com>
2015-02-24 09:49:05 -0500

    add new endpoints

--- urls.py
+++ urls.py
@@ -9,11 +9,15 @@ from rest_framework.routers import DefaultRouter
 # API router
 router = DefaultRouter()
 router.register(r'users', user_views.UserViewSet)
-router.register(r'characters', charman_views.CharacterViewSet)
 router.register(r'sizes', charman_views.SizeViewSet)
 router.register(r'races', charman_views.RaceViewSet)
 router.register(r'skills', charman_views.SkillViewSet)
-router.register(r'character_classes', charman_views.CharacterClassViewSet)
+router.register(r'classes', charman_views.CharacterClassViewSet)
+router.register(r'features', charman_views.FeatureViewSet)
+router.register(r'feats', charman_views.FeatViewSet)
+router.register(r'characters', charman_views.CharacterViewSet)
+router.register(r'classlevels', charman_views.ClassLevelViewSet)
+
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
