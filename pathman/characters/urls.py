a2401f25a5952f7fe9a258f383dacf7a54fafbc0
Michael D. Hoyle <hoylemd@gmail.com>
2016-02-13 18:58:10 -0500

    Add detail and list views

--- pathman/characters/urls.py
+++ pathman/characters/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.CharacterIndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.CharacterDetailView.as_view(),
+        name='detail'),
     url(r'^new$', views.CharacterCreateView.as_view(), name='new')
 ]
--- pathman/characters/views.py
+++ pathman/characters/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Character
 
@@ -12,3 +14,11 @@ class CharacterCreateView(CreateView):
     model = Character
     fields = ['name', 'hp', 'strength', 'dexterity', 'constitution',
               'intelligence', 'wisdom', 'charisma']
+
+
+class CharacterIndexView(ListView):
+    model = Character
+
+
+class CharacterDetailView(DetailView):
+    model = Character
