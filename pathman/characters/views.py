0cec261aec079d4e69724dfd314ad81b6ac25e87
Michael D. Hoyle <hoylemd@gmail.com>
2016-02-13 17:17:22 -0500

    Add Character create view

--- pathman/characters/views.py
+++ pathman/characters/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+
+from .models import Character
 
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+
+class CharacterCreateView(CreateView):
+    model = Character
+    fields = ['name', 'hp']
