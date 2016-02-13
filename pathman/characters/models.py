9875b34e6cde3fea577f7e9e8eddcf6ecaff87fa
Michael D. Hoyle <hoylemd@gmail.com>
2016-02-13 18:39:38 -0500

    Fill out the character model slightly

--- pathman/characters/models.py
+++ pathman/characters/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 from model_utils.models import TimeStampedModel
 
 
 class InstrumentedModel(TimeStampedModel):
-    owner = models.ForeignKey(User)
-
     class Meta:
         abstract = True
 
