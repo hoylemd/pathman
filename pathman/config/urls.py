7e9eeecb55daf433bd5de07ca80def66fdce3c64
Michael D. Hoyle <hoylemd@gmail.com>
2016-02-13 18:40:37 -0500

    Add admin site

--- pathman/config/urls.py
+++ pathman/config/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^characters/', include('characters.urls'), name='index'),
+    url(r'^admin/', admin.site.urls)
 ]
