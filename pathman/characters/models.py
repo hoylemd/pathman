3eab960832fe00926a056e0d9c02009a6f0fdea8
Michael D. Hoyle <hoylemd@gmail.com>
2016-02-13 16:32:20 -0500

    Add Character and Instrumented models
    InstrumentedModel tracks changed/created staes, and the current owner.

--- pathman/characters/models.py
+++ pathman/characters/models.py
@@ -1,4 +1,24 @@
-from django import models
+from django.db import models
+from django.contrib.auth.models import User
 
-class Character(models.model):
+from model_utils.models import TimeStampedModel
+
+
+class InstrumentedModel(TimeStampedModel):
+    owner = models.ForeignKey(User)
+
+    class Meta:
+        abstract = True
+
+
+class Character(InstrumentedModel):
     name = models.CharField(max_length=200)
+
+    hp = models.IntegerField()
+
+    strength = models.IntegerField()
+    dexterity = models.IntegerField()
+    constitution = models.IntegerField()
+    intelligence = models.IntegerField()
+    wisdom = models.IntegerField()
+    charisma = models.IntegerField()
