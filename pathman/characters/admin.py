19c6c49b573c8c7234090f001ab07dff974d9d4f
Michael D. Hoyle <hoylemd@gmail.com>
2016-02-15 23:38:02 -0500

    Add classlevel to admin

--- pathman/characters/admin.py
+++ pathman/characters/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Character, CharacterClass
+from .models import Character, CharacterClass, ClassLevel
 
 admin.site.register(Character)
 admin.site.register(CharacterClass)
+admin.site.register(ClassLevel)
