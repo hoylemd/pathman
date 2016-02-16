0305a712e0cb424464eb6f90973c8b57c627116a
Michael D. Hoyle <hoylemd@gmail.com>
2016-02-16 00:02:34 -0500

    Move classlevel admin to character admin

--- pathman/characters/admin.py
+++ pathman/characters/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import Character, CharacterClass, ClassLevel
 
-admin.site.register(Character)
+
+class ClassLevelInline(admin.TabularInline):
+    model = ClassLevel
+    extra = 0
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    inlines = (ClassLevelInline,)
+
+
+admin.site.register(Character, CharacterAdmin)
 admin.site.register(CharacterClass)
-admin.site.register(ClassLevel)
