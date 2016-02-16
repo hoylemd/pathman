59d046e090acbee02e63d8840bb20673406decbc
Michael D. Hoyle <hoylemd@gmail.com>
2016-02-15 21:41:44 -0500

    Add ability modifiers to Character model

--- pathman/characters/models.py
+++ pathman/characters/models.py
@@ -39,6 +39,10 @@ class CharacterClass(InstrumentedModel):
         return self.name
 
 
+def ability_modifier(score):
+    return score / 2 - 5
+
+
 class Character(InstrumentedModel):
     name = models.CharField(max_length=200)
 
@@ -52,6 +56,30 @@ class Character(InstrumentedModel):
     charisma = models.IntegerField()
 
     classes = models.ManyToManyField(CharacterClass, through='ClassLevel')
+
+    @property
+    def strength_mod(self):
+        return ability_modifier(self.strength)
+
+    @property
+    def dexterity_mod(self):
+        return ability_modifier(self.dexterity)
+
+    @property
+    def constitution_mod(self):
+        return ability_modifier(self.constitution)
+
+    @property
+    def intelligence_mod(self):
+        return ability_modifier(self.intelligence)
+
+    @property
+    def wisdom_mod(self):
+        return ability_modifier(self.wisdom)
+
+    @property
+    def charisma_mod(self):
+        return ability_modifier(self.charisma)
 
     def save(self, *args, **kwargs):
         slug_name = slugify(self.name)
