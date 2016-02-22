ec0cc99986a9a9567085dd1eae7bf200a335f688
Michael D. Hoyle <hoylemd@gmail.com>
2016-02-22 00:09:26 -0500

    Move url utility functions into util.urls

--- pathman/races/urls.py
+++ pathman/races/urls.py
@@ -3,37 +3,11 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import RedirectView
 
+from util.urls import slug_url
+
 from . import models
 
 app_name = 'races'
-
-
-def detail_url(identifier, pattern, prefix='', extension=''):
-    """
-    Generates a url regex based on the passed identifier column name
-    and the expected pattern.
-
-    :param identifier: The model field used to select the record
-        (e.g. 'pk' or 'slug')
-    :type identifier: string
-
-    :param pattern: Regular expression pattern for identifiers
-    :type pattern: string or regexp
-
-    :param prefix: Optional prefix on the url before the identifier
-    :type prefix: string or regexp
-
-    :rtype: a detail url pattern
-    """
-    suffix = extension if extension else '/?'
-    return r'^{}(?P<{}>{}){}$'.format(prefix, identifier, pattern, suffix)
-
-
-def slug_url(prefix='', extension=''):
-    return detail_url('slug', '[a-z\-_]+',
-                      prefix=prefix, extension=extension)
-
-
 urlpatterns = [
     # 'races/
     url(r'^$',
