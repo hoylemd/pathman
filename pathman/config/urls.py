8841f4e8a32b350e76a1cd4b8ddacf026ecec424
Michael D. Hoyle <hoylemd@gmail.com>
2016-03-06 18:30:38 -0500

    Add home page

--- pathman/config/urls.py
+++ pathman/config/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='core/index.html')),
     url(r'^characters/', include('characters.urls', namespace='characters')),
     url(r'^classes/', include('classes.urls', namespace='classes')),
     url(r'^races/', include('races.urls', namespace='races')),
