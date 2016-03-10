411cebc4da8c6df9748199dce0c33314f2052a86
Michael D. Hoyle <hoylemd@gmail.com>
2016-03-09 22:02:17 -0500

    Add absolute url to race

--- pathman/races/models.py
+++ pathman/races/models.py
@@ -1,5 +1,8 @@
+from django.core.urlresolvers import reverse
+
 from util.models import InstrumentedModel
 
 
 class Race(InstrumentedModel):
-    pass
+    def get_absolute_url(self):
+        return reverse('race:detail', args=[str(self.slug)])
